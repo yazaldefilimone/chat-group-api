4ed85facb5405b64b239ceeda395b0a23d0e2ac2
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-29 00:09:16 +0200

    feat: create entity user

--- src/domain/entities/user/index.ts
+++ src/domain/entities/user/index.ts
@@ -1,1 +1,11 @@
-export class User {}
+import { user, userCreate } from './protocols';
+import { v4 as uuid } from 'uuid';
+export class User {
+  static build(data: user): userCreate {
+    return {
+      ...data,
+      id: uuid(),
+      created_at: new Date().toISOString(),
+    };
+  }
+}
