22820dbbfed16ecb9b88cf19a90fde3f1e956bc1
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-29 00:08:59 +0200

    fix: change names

---
+++ src/domain/entities/user/protocols/index.ts
@@ -0,0 +1,19 @@
+export type user = {
+  name: string;
+  email: string;
+  password: string;
+};
+export type userSimples = {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+  created_at: string;
+};
+export type userCreate = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  created_at: string;
+};
