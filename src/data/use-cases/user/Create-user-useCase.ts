1f2ff216d08de0c8fab7a1b7abee26036ad9bdec
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-29 11:39:35 +0200

    feat: implemenes create user

---
+++ src/data/use-cases/user/Create-user-useCase.ts
@@ -0,0 +1,32 @@
+import { IEncoder } from '@/data/protocols/encoder';
+import { IUserRepository } from '@/data/repositories/user';
+import { AlreadyExistsError } from '@/domain/errors';
+import { User } from '@/domain/user/entity/user';
+import { ICreateUserUseCase } from '@/domain/user/use-cases';
+
+import { left, right } from '@/shared/error-handler/either';
+
+export class CreateUserUseCase implements ICreateUserUseCase {
+  private readonly userRepository: IUserRepository;
+  private readonly encoder: IEncoder;
+  constructor(userRepository: IUserRepository, encoder: IEncoder) {
+    this.userRepository = userRepository;
+    this.encoder = encoder;
+  }
+
+  async perform(data: ICreateUserUseCase.Input): ICreateUserUseCase.Output {
+    const userBuild = new User().build(data);
+
+    if (userBuild.isLeft()) return left(userBuild.value);
+
+    const isExist = await this.userRepository.findByEmail({ email: userBuild.value.email });
+
+    if (!isExist) return left(new AlreadyExistsError('user'));
+
+    userBuild.value.password = await this.encoder.encode({ value: userBuild.value.password });
+
+    const user = await this.userRepository.add(userBuild.value);
+
+    return right(user);
+  }
+}
