938fba852d81df1e3b738b0ee9c3b065c4d7de5b
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-29 10:37:51 +0200

    feat: create types of entityu

---
+++ src/domain/user/protocols/index.ts
@@ -0,0 +1,26 @@
+import { InvalidParamsError } from '@/domain/errors/InvalidParamsError';
+import { Either } from '@/shared/error-handler/either';
+
+export type user = {
+  name: string;
+  email: string;
+  password: string;
+};
+export type userSimples = {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+  created_at: string;
+};
+export type userCreate = {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  created_at: string;
+};
+type CreateUserError = InvalidParamsError;
+type CreateUserSucessFully = userCreate;
+
+export type ResponseBuildUser = Either<CreateUserError, CreateUserSucessFully>;
