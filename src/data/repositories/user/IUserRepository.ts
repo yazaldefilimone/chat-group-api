1f4341273e995b544ef83b081735d514ba0fa1b8
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-29 11:28:55 +0200

    fix: change name params

--- src/data/repositories/user/IUserRepository.ts
+++ src/data/repositories/user/IUserRepository.ts
@@ -3,9 +3,9 @@ import { userRepo } from './protocols';
 export interface IUserRepository {
   add: (data: IUserRepository.Input) => IUserRepository.Output<userRepo>;
   findByName: ({ name }: { name: string }) => IUserRepository.Output<userRepo | undefined>;
-  findById: ({ name }: { name: string }) => IUserRepository.Output<userRepo | undefined>;
-  findByEmail: ({ name }: { name: string }) => IUserRepository.Output<userRepo | undefined>;
-  delete: ({ name }: { name: string }) => IUserRepository.Output<void>;
+  findById: ({ id }: { id: string }) => IUserRepository.Output<userRepo | undefined>;
+  findByEmail: ({ email }: { email: string }) => IUserRepository.Output<userRepo | undefined>;
+  delete: ({ id }: { id: string }) => IUserRepository.Output<void>;
   findAll: () => IUserRepository.Output<userRepo[]>;
   update: (data: IUserRepository.Input) => IUserRepository.Output<userRepo>;
 }
