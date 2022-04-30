ec9c6b049a2259313e003dc2cf0d6fa49082e3d7
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-30 20:42:45 +0200

    feat: create interface of login user

---
+++ src/domain/user/use-cases/Ilogin-user-usecase.ts
@@ -0,0 +1,10 @@
+import { ResponseUser, user } from '../protocols';
+
+export interface ILoginUserUseCase {
+  perform: (data: ILoginUserUseCase.Input) => ILoginUserUseCase.Output;
+}
+
+export namespace ILoginUserUseCase {
+  export type Input = Omit<user, 'name'>;
+  export type Output = Promise<ResponseUser>;
+}
--- src/domain/user/use-cases/index.ts
+++ src/domain/user/use-cases/index.ts
@@ -1,1 +1,2 @@
 export * from './Icreate-user-usecase';
+export * from './Ilogin-user-usecase';
