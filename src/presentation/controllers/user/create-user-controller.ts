43373edc5856b3e55e2c628c62011f31440bf01f
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-30 16:50:13 +0200

    feat: create create user controller

---
+++ src/presentation/controllers/user/create-user-controller.ts
@@ -0,0 +1,23 @@
+import { ICreateUserUseCase } from '@/domain/user/use-cases';
+import { IController } from '@/presentation/protocols';
+
+import { Request, Response } from 'express';
+
+export class CreateUserController implements IController {
+  private readonly createUserUseCase: ICreateUserUseCase;
+  constructor(createUserUseCase: ICreateUserUseCase) {
+    this.createUserUseCase = createUserUseCase;
+  }
+
+  async handler(request: Request, response: Response): Promise<Response> {
+    const { name, email, password } = request.body;
+
+    const sucessOrFailed = await this.createUserUseCase.perform({ name, email, password });
+
+    if (sucessOrFailed.isLeft()) {
+      return response.status(400).json({ message: sucessOrFailed.value });
+    }
+
+    return response.status(200).json(sucessOrFailed.value);
+  }
+}
