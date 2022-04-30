7e9f3540a6ee3bbc89e12f77f94bb108139d3516
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-30 22:51:11 +0200

    feat: create route of FindAllUser

--- src/main/routes/user/index.ts
+++ src/main/routes/user/index.ts
@@ -1,8 +1,9 @@
-import { createUserFactory, finByNameUserFactory, loginUserFactory } from '@/main/factories/user';
+import { createUserFactory, finByNameUserFactory, loginUserFactory, findAllUserFactory } from '@/main/factories/user';
 import { Router } from 'express';
 
 const userRoutes = Router();
 userRoutes.post('/signup', createUserFactory);
 userRoutes.post('/login', loginUserFactory);
 userRoutes.get('/name/:name', finByNameUserFactory);
+userRoutes.get('/all', findAllUserFactory);
 export { userRoutes };
