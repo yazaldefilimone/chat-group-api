90dac75e1ed22276364e3c5dbfe6426ef313f8e0
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-30 16:16:22 +0200

    fix: change undefiend for null

--- src/data/repositories/user/IUserRepository.ts
+++ src/data/repositories/user/IUserRepository.ts
@@ -2,9 +2,9 @@ import { userRepo } from './protocols';
 
 export interface IUserRepository {
   add: (data: IUserRepository.Input) => IUserRepository.Output<userRepo>;
-  findByName: ({ name }: { name: string }) => IUserRepository.Output<userRepo | undefined>;
-  findById: ({ id }: { id: string }) => IUserRepository.Output<userRepo | undefined>;
-  findByEmail: ({ email }: { email: string }) => IUserRepository.Output<userRepo | undefined>;
+  findByName: ({ name }: { name: string }) => IUserRepository.Output<userRepo[] | null>;
+  findById: ({ id }: { id: string }) => IUserRepository.Output<userRepo | null>;
+  findByEmail: ({ email }: { email: string }) => IUserRepository.Output<userRepo | null>;
   delete: ({ id }: { id: string }) => IUserRepository.Output<void>;
   findAll: () => IUserRepository.Output<userRepo[]>;
   update: (data: IUserRepository.Input) => IUserRepository.Output<userRepo>;
