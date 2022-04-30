5104487efa4a067c6708ad1a91f86fd84bdd0e3e
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-30 22:44:44 +0200

    style: add export method in files

--- src/data/use-cases/user/index.ts
+++ src/data/use-cases/user/index.ts
@@ -1,3 +1,4 @@
 export * from './Create-user-usecase';
 export * from './Login-user-usecase';
 export * from './FindByName-user-usecase';
+export * from './FindAll-user-usecase';
--- src/presentation/controllers/user/index.ts
+++ src/presentation/controllers/user/index.ts
@@ -1,3 +1,4 @@
 export * from './create-user-controller';
 export * from './login-user-controller';
 export * from './findByName-user-controller';
+export * from './findAll-user-controller';
