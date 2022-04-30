91ca42ae571b8a7f6b3cd2e4fc823bd4f72c9291
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-30 21:59:40 +0200

    feat: create route of login user

--- src/main/routes/user/index.ts
+++ src/main/routes/user/index.ts
@@ -1,6 +1,7 @@
-import { createUserFactory } from '@/main/factories/user';
+import { createUserFactory, loginUserFactory } from '@/main/factories/user';
 import { Router } from 'express';
 
 const userRoutes = Router();
 userRoutes.post('/signup', createUserFactory);
+userRoutes.post('/login', loginUserFactory);
 export { userRoutes };
