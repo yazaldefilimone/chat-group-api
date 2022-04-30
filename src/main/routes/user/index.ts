58dcbe9fb530f461a61457342a8f563346caed60
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-04-30 22:35:45 +0200

    feat: create router of findByName

--- src/main/routes/user/index.ts
+++ src/main/routes/user/index.ts
@@ -1,7 +1,8 @@
-import { createUserFactory, loginUserFactory } from '@/main/factories/user';
+import { createUserFactory, finByNameUserFactory, loginUserFactory } from '@/main/factories/user';
 import { Router } from 'express';
 
 const userRoutes = Router();
 userRoutes.post('/signup', createUserFactory);
 userRoutes.post('/login', loginUserFactory);
+userRoutes.get('/name/:name', finByNameUserFactory);
 export { userRoutes };
