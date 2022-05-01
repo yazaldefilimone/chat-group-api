09fdb0e43433273d136cea9e0266fe0ac601b1c8
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-01 10:27:25 +0200

    feat: create method of cryptography data

---
+++ src/data/protocols/cryptography/index.ts
@@ -0,0 +1,12 @@
+export interface ICryptography {
+  encrypt: (data: ICryptography.Input) => ICryptography.Output;
+  decrypt: boolean;
+}
+export namespace ICryptography {
+  export type Input = {
+    data: string;
+  };
+  export type Output = {
+    data: string;
+  };
+}
