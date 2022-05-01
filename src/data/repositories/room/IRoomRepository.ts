a3a29d1f7b750e83cc26c93d02a6b14431b6ab24
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-01 10:50:16 +0200

    feat: create method of findByNames

--- src/data/repositories/room/IRoomRepository.ts
+++ src/data/repositories/room/IRoomRepository.ts
@@ -2,7 +2,8 @@ import { roomRepo } from './protocols';
 
 export interface IRoomRepository {
   add: (data: IRoomRepository.Input) => IRoomRepository.Output<roomRepo>;
-  findByName: ({ name }: { name: string }) => IRoomRepository.Output<roomRepo[] | null>;
+  findByName: ({ name }: { name: string }) => IRoomRepository.Output<roomRepo | null>;
+  findByNames: ({ name }: { name: string }) => IRoomRepository.Output<roomRepo[] | null>;
   findById: ({ id }: { id: string }) => IRoomRepository.Output<roomRepo | null>;
   delete: ({ id }: { id: string }) => IRoomRepository.Output<void>;
   findAll: () => IRoomRepository.Output<roomRepo[]>;
