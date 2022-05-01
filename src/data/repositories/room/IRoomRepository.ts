02e11add665c8a884665af5f78d5620f5e02fa62
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-01 18:28:43 +0200

    fix: add type return

--- src/data/repositories/room/IRoomRepository.ts
+++ src/data/repositories/room/IRoomRepository.ts
@@ -3,7 +3,7 @@ import { roomRepo, roomRepoAll } from './protocols';
 export interface IRoomRepository {
   add: (data: IRoomRepository.Input) => IRoomRepository.Output<roomRepoAll>;
   findByName: ({ name }: { name: string }) => IRoomRepository.Output<roomRepo | null>;
-  findByNames: ({ name }: { name: string }) => IRoomRepository.Output<roomRepo | null>;
+  findByNames: ({ name }: { name: string }) => IRoomRepository.Output<roomRepo[] | null>;
   findById: ({ id }: { id: string }) => IRoomRepository.Output<roomRepoAll | null>;
   delete: ({ id }: { id: string }) => IRoomRepository.Output<void>;
   findAll: () => IRoomRepository.Output<roomRepo[]>;
