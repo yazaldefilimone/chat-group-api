21833eafd770c0b7dc7065a8c15660eeb63ef19e
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-01 11:19:23 +0200

    feat: add milldreware in routes

--- src/main/routes/user/index.ts
+++ src/main/routes/user/index.ts
@@ -1,9 +1,10 @@
 import { createUserFactory, finByNameUserFactory, loginUserFactory, findAllUserFactory } from '@/main/factories/user';
+import { authUserJwtMiddleware } from '@/main/routes/middlewares';
 import { Router } from 'express';
 
 const userRoutes = Router();
 userRoutes.post('/signup', createUserFactory);
 userRoutes.post('/login', loginUserFactory);
-userRoutes.get('/name/:name', finByNameUserFactory);
-userRoutes.get('/all', findAllUserFactory);
+userRoutes.get('/name/:name', authUserJwtMiddleware, finByNameUserFactory);
+userRoutes.get('/all', authUserJwtMiddleware, findAllUserFactory);
 export { userRoutes };
