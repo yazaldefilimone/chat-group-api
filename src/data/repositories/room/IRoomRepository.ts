05328227fd3509d7831a6ce18d0e6754c7c80bd4
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-01 10:14:05 +0200

    feat: create interface of roomRepository

---
+++ src/data/repositories/room/IRoomRepository.ts
@@ -0,0 +1,14 @@
+import { roomRepo } from './protocols';
+
+export interface IRoomRepository {
+  add: (data: IRoomRepository.Input) => IRoomRepository.Output<roomRepo>;
+  findByName: ({ name }: { name: string }) => IRoomRepository.Output<roomRepo[] | null>;
+  findById: ({ id }: { id: string }) => IRoomRepository.Output<roomRepo | null>;
+  delete: ({ id }: { id: string }) => IRoomRepository.Output<void>;
+  findAll: () => IRoomRepository.Output<roomRepo[]>;
+}
+
+export namespace IRoomRepository {
+  export type Input = roomRepo;
+  export type Output<T> = Promise<T>;
+}
