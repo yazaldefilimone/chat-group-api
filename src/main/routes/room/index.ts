44fbed4c8d092718bf8d73db1d548367087ced68
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-01 18:30:52 +0200

    feat: create router of list room

--- src/main/routes/room/index.ts
+++ src/main/routes/room/index.ts
@@ -1,7 +1,8 @@
-import { createRoomFactory } from '@/main/factories/room';
+import { createRoomFactory, finByIdRoomFactory } from '@/main/factories/room';
 import { authUserJwtMiddleware } from '@/main/routes/middlewares';
 import { Router } from 'express';
 
 const roomRoutes = Router();
 roomRoutes.post('/room', authUserJwtMiddleware, createRoomFactory);
+roomRoutes.get('/room/:id', authUserJwtMiddleware, finByIdRoomFactory);
 export { roomRoutes };
