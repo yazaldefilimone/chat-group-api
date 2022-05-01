722ce9ec3e8b2718f3d33aa3b4b70e69c7e1dad5
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-01 18:13:39 +0200

    feat: remove omit method

--- src/data/repositories/room/IRoomRepository.ts
+++ src/data/repositories/room/IRoomRepository.ts
@@ -1,12 +1,12 @@
 import { roomRepo, roomRepoAll } from './protocols';
 
 export interface IRoomRepository {
-  add: (data: IRoomRepository.Input) => IRoomRepository.Output<Omit<roomRepoAll, 'userId'>>;
-  findByName: ({ name }: { name: string }) => IRoomRepository.Output<Omit<roomRepo, 'userId'> | null>;
-  findByNames: ({ name }: { name: string }) => IRoomRepository.Output<Omit<roomRepo, 'userId'>[] | null>;
-  findById: ({ id }: { id: string }) => IRoomRepository.Output<Omit<roomRepo, 'userId'> | null>;
+  add: (data: IRoomRepository.Input) => IRoomRepository.Output<roomRepoAll>;
+  findByName: ({ name }: { name: string }) => IRoomRepository.Output<roomRepo | null>;
+  findByNames: ({ name }: { name: string }) => IRoomRepository.Output<roomRepo | null>;
+  findById: ({ id }: { id: string }) => IRoomRepository.Output<roomRepoAll | null>;
   delete: ({ id }: { id: string }) => IRoomRepository.Output<void>;
-  findAll: () => IRoomRepository.Output<Omit<roomRepo, 'userId'>[]>;
+  findAll: () => IRoomRepository.Output<roomRepo[]>;
 }
 
 export namespace IRoomRepository {
