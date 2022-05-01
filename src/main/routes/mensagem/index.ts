363b32c6abacc6d74693f05700411daf0e22de2c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-01 17:39:04 +0200

    feat: create router of find mensagemby room

--- src/main/routes/mensagem/index.ts
+++ src/main/routes/mensagem/index.ts
@@ -1,7 +1,8 @@
-import { createMensagemFactory } from '@/main/factories/mensagen';
+import { createMensagemFactory, findByRoomMensagemFactory } from '@/main/factories/mensagen';
 import { authUserJwtMiddleware } from '@/main/routes/middlewares';
 import { Router } from 'express';
 
 const mensagemRoutes = Router();
 mensagemRoutes.post('/mensagem', authUserJwtMiddleware, createMensagemFactory);
+mensagemRoutes.get('/mensagem/:roomId', authUserJwtMiddleware, findByRoomMensagemFactory);
 export { mensagemRoutes };
