bc50a618679f4dbd2747e265f92b70f8c6aeed93
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-01 10:07:06 +0200

    feat: create protocols of room entity

---
+++ src/domain/room/protocols/index.ts
@@ -0,0 +1,24 @@
+import { AlreadyExistsError, InvalidCredentials, InvalidParamsError, NotFoundError } from '@/domain/errors';
+import { Either } from '@/shared/error-handler/either';
+
+export type room = {
+  name: string;
+  description: string;
+};
+export type roomSimples = {
+  id?: string;
+  name: string;
+  description: string;
+  created_at: string;
+};
+export type roomCreate = {
+  id: string;
+  name: string;
+  description: string;
+  created_at: string;
+};
+
+type CreateRoomError = InvalidParamsError | AlreadyExistsError | NotFoundError;
+
+export type ResponseBuildRoom = Either<InvalidParamsError, roomCreate>;
+export type ResponseRoom = Either<CreateRoomError, roomCreate>;
